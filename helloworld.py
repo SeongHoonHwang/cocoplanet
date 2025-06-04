a0f5c7cdc3f48e001c0c8dda7914ec709a79072b
Seonghoon Hwang <seonghoon.hwang@neuromeka.com>
2025-06-04 12:25:09 +0900

    second commit

--- helloworld.py
+++ helloworld.py
@@ -2,3 +2,13 @@
 print("Hello World!")
 print("Life is too short, you need a Python")
 print("Hello Hello Hello")
+
+
+class unit:
+    def __init__(self):
+        pass
+
+class AttackUnit:
+    def __init__(self):
+        pass
+
